332bb9b10b27ea57845d7ceca968b5db73ab9652
Chiara Relandini <chiara.relandini@gmail.com>
2024-10-22 11:13:10 +0200

    Minor fix: typo

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // DO NOT COMMIT AWS resources IDs to your git repository
 const config = {
-    "aws_project_region": "{aws_region}", // The same you have used as aws_region in cdk/config.json
-    "bedrock_region": "{bedrock_region}", // The same you have used as bedrock_region in cdk/config.json
+    "aws_project_region": "{aws_region}", // The same you have used as aws_region in cdk/bin/config.json
+    "bedrock_region": "{bedrock_region}", // The same you have used as bedrock_region in cdk/bin/config.json
     "APIGatewayId": "{APIGatewayId}", // From CloudFormation outputs
     "BucketS3Name": "{BucketS3Name}", // From CloudFormation outputs
     "CognitoIdentityPoolId": "{CognitoIdentityPoolId}", // From CloudFormation outputs
